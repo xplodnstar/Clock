7802d052fdd028349985ea8946e8b4b896ace41d
Nova <xplodnstar@live.com>
2019-04-01 16:11:18 -0700

    adding hard mode files

---
+++ Hard/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // setInterval(function () {
+    //     var r
+    //     var g
+    //     var b
+    //     for (r = 0; r <= 255; r + 10) {
+    //         for (g = 0; g <= 255; g + 10) {
+    //             for (b = 0; b <= 255; b + 10) {
+    //                 var hue = "#" + r.toString(16) + g.toString(16) + b.toString(16)
+
+    //                 $(".color").html(hue);
+
+    //                 console.log(hue)
+    //             }
+    //         }
+    //     }
+
+    // }, 1)
+
+});
+
